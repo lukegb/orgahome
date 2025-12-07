89d54f0a314bc3f94242486c9a546c1fd36626d0
Luke Granger-Brown <git@lukegb.com>
2025-12-07 20:17:55 +0000

    Use starlette for serving static files
    ...I'll probably actually rip all the Flask code out and replace it with
    Starlette. Let's see.

--- orgahome/cli.py
+++ orgahome/cli.py
@@ -1,16 +1,30 @@
 import multiprocessing
+import typing
 
 import click
 import uvicorn
 from a2wsgi import WSGIMiddleware
 from flask import Flask
 from flask.cli import FlaskGroup
+from starlette.applications import Starlette
+from starlette.routing import ASGIApp, Mount
+from starlette.staticfiles import StaticFiles
 
 from orgahome.app import app
 
 
 def create_app():
     return app
+
+
+def create_asgi_app():
+    flask_app = create_app()
+    flask_app.static_folder = None
+    routes = [
+        Mount("/static", app=StaticFiles(packages=[("orgahome", "static")])),
+        Mount("/", app=typing.cast(ASGIApp, WSGIMiddleware(flask_app))),
+    ]
+    return Starlette(routes=routes)
 
 
 @click.group(cls=FlaskGroup, create_app=create_app)
@@ -32,7 +46,7 @@ def uvicorn_command(host, port, workers, forwarded_allow_ips):
     config = uvicorn.Config(
         host=host,
         port=port,
-        app=WSGIMiddleware(create_app()),
+        app=create_asgi_app(),
         workers=default_workers() if workers is None else workers,
         forwarded_allow_ips=forwarded_allow_ips,
     )
