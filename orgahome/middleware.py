bdb1854da4db350b75e0d98600bdb95e023d2757
Luke Granger-Brown <git@lukegb.com>
2025-12-08 21:17:34 +0000

    Expire the session when the id_token from uffd expires

--- orgahome/middleware.py
+++ orgahome/middleware.py
@@ -1,3 +1,5 @@
+import time
+
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.requests import Request
 
@@ -5,6 +7,11 @@ from starlette.requests import Request
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         user = request.session.get("user")
+        if user:
+            expires_at = user.get("exp")
+            if not expires_at or expires_at < time.time():
+                request.session.clear()
+                user = None
         if not user:
             # Store the current URL to redirect back to after login
             request.session["next"] = str(request.url)
--- orgahome/views/auth.py
+++ orgahome/views/auth.py
@@ -1,9 +1,12 @@
+from authlib.integrations.starlette_client import OAuth
 from starlette.requests import Request
 from starlette.responses import RedirectResponse
 
 
 async def authorize(request: Request):
-    token = await request.state.oauth.uffd.authorize_access_token(request)
+    oauth: OAuth = request.state.oauth
+    token = await oauth.uffd.authorize_access_token(request)
     request.session["user"] = token["userinfo"]
+    request.session["id_token"] = token["id_token"]
     next_url = request.session.pop("next", "/")
     return RedirectResponse(url=next_url)
